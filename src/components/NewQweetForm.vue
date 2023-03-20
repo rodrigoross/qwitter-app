4d6edf8391f5d0d9cdad2cc20271b271300e3245
Rodrigo de Sousa <rodrigo.ross.comp@gmail.com>
2023-03-20 16:00:04 -0400

    feat: :lipstick: component de qweet

---
+++ src/components/NewQweetForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="q-py-lg q-px-sm row items-end q-col-gutter-md">
+    <div class="col">
+      <q-input
+        class="new-qweet"
+        bottom-slots
+        v-model="newQweetContent"
+        placeholder="What's happening"
+        counter
+        maxlength="280"
+        autogrow
+      >
+        <template v-slot:before>
+          <q-avatar size="xl">
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+          </q-avatar>
+        </template>
+      </q-input>
+    </div>
+    <div class="col col-shrink">
+      <q-btn
+        class="q-mb-lg"
+        no-caps
+        unelevated
+        rounded
+        color="primary"
+        label="Qweet"
+        :disable="!newQweetContent"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const newQweetContent = ref('');
+</script>
+
+<style scoped lang="sass">
+.new-qweet
+  textarea
+    font-size: 19px
+    line-height: 1.4rem !important
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,17 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <h5>Home</h5>
+  <q-page>
+    <new-qweet-form />
+    <q-separator class="divider" size="10px" color="grey-2" />
   </q-page>
 </template>
+
+<script setup lang="ts">
+import NewQweetForm from 'src/components/NewQweetForm.vue';
+</script>
+
+<style lang="sass">
+.divider
+  border-top: 1px solid
+  border-bottom: 1px solid
+  border-color: $grey-4
+</style>
