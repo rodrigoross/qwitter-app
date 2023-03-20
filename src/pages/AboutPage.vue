db805b30ce59192d76e714f2037f6f9018a79c80
Rodrigo de Sousa <rodrigo.ross.comp@gmail.com>
2023-03-20 15:36:12 -0400

    refactor: :lipstick: pagina sobre

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,5 +1,18 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, laborum
+        hic laudantium dolore dolorem rem iste similique voluptatibus numquam
+        vero et tenetur aliquid totam iure facilis, assumenda ex soluta non?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum vitae
+        aspernatur, expedita tenetur odio modi eligendi architecto ipsam porro
+        veritatis assumenda deserunt consectetur laudantium rerum, repellat quae
+        nihil facilis numquam!
+      </p>
+    </div>
   </q-page>
 </template>
